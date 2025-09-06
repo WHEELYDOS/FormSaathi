fe5a17fc67285be7360dddc3b3a0aec971b53da2
WHEELYDOS <111660240+WHEELYDOS@users.noreply.github.com>
2025-09-06 16:18:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
+    Server: {
+        host: true
+    }
     return {
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
